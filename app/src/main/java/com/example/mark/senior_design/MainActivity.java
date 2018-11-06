ae3edd49ae014250f3b0dee112a6f07caa056ac7
MarkWatson95 <markle951@yahoo.com>
2018-11-06 15:37:19 -0500

    Basic ListView structure for Task List page

--- app/src/main/java/com/example/mark/senior_design/MainActivity.java
+++ app/src/main/java/com/example/mark/senior_design/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.mark.senior_design;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        lv = (ListView) findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("android");
+        arrayList.add("Study Science");
+        arrayList.add("Learn to Play the Piano");
+        arrayList.add("Read new book");
+
+
+        ArrayAdapter AA = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+
+        lv.setAdapter(AA);
     }
+
+
 }
