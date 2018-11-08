a6783b25e4462d2d0b6b95d7afcb6b32f6695df7
Khanh <knguyen2@stevens.edu>
2018-11-08 11:47:30 -0800

    Welcome Page and Button to Task view ( for testing )

--- app/src/main/java/com/example/mark/senior_design/WelcomePage.java
+++ app/src/main/java/com/example/mark/senior_design/WelcomePage.java
@@ -1,5 +1,6 @@
 package com.example.mark.senior_design;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +11,12 @@ public class WelcomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcomepage);
         final Button button = findViewById(R.id.welcome_button);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-
-            }});
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomePage.this, MainActivity.class));
+            }
+        });
     }
 }
